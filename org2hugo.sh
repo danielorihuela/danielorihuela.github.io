ee4ffb53ca0069fa2b63940807737009fa729d85
dani <danielorihuelarodriguez@gmail.com>
2023-02-07 14:40:45 +0100

    feat: store all documents inside one folder

--- org2hugo.sh
+++ org2hugo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export HUGO_BASE_DIR=`pwd`
-export POSTS_ORG_SRC=`pwd`/posts
-export ABOUT_ORG_SRC=`pwd`/about
-export OPEN_SOURCE_ORG_SRC=`pwd`/open-source
+export POSTS_ORG_SRC=`pwd`/org-content/posts
+export ABOUT_ORG_SRC=`pwd`/org-content/about
+export OPEN_SOURCE_ORG_SRC=`pwd`/org-content/open-source
 HOME=/tmp/emacs-build
 
 mkdir -p $HOME
