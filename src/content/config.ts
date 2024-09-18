da3645822c769aa08735f286ba82c9ed39c226cb
dani <danielorihuelarodriguez@gmail.com>
2024-09-18 22:14:57 +0200

    feat: add cover images

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,13 +3,13 @@ import { defineCollection, z } from 'astro:content';
 const blog = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
-	schema: z.object({
+	schema: ({ image }) => z.object({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
 		publishdate: z.coerce.date(),
 		updateddate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		cover: image()
 	}),
 });
 
