4880dab008de154fca22f50670f80c52a943d355
dani <danielorihuelarodriguez@gmail.com>
2024-10-19 22:15:05 +0200

    feat: update site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://danielorihuela.github.io/',
+    site: 'https://danielorihuela.dev/',
     integrations: [mdx(), sitemap()],
     markdown: {
         syntaxHighlight: 'prism',
