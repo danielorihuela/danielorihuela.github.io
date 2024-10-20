9b9a05ce1c1e52728927ac57e396f05b105321b3
dani <danielorihuelarodriguez@gmail.com>
2024-10-20 13:01:58 +0200

    feat: update main site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Daniel Orihuela';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Software engineer passionate about Cybersecurity, AI and Distributed Systems. Constantly learning and evolving.';
