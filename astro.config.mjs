6329e7f438539e4f51478aec300f09b5c2d5d0d1
dani <danielorihuelarodriguez@gmail.com>
2024-11-12 21:26:47 +0100

    feat: add rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://danielorihuela.dev/',
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), tailwind()],
     markdown: {
         syntaxHighlight: 'prism',
     },
