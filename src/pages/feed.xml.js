8dd7663e3de398a9d0c7bfbfca8693ea9e4a2b93
dani <danielorihuelarodriguez@gmail.com>
2024-11-12 21:45:34 +0100

    fix: feed xml

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -8,13 +8,19 @@ export async function GET(context) {
         title: 'Daniel Orihuela\'s Blog',
         description: 'Dive into the fascinating world of software development, security, and AI with Daniel Orihuela.',
         site: context.site,
+        xmlns: {
+            'atom': 'http://www.w3.org/2005/Atom'
+        },
+        customData:
+            `<atom:link href="${new URL('feed.xml', context.site)}" rel="self" type="application/rss+xml" />`
+        ,
         items: blogPosts.map((post) => ({
             title: post.data.title,
             pubDate: post.data.pubDate,
             description: post.data.description,
             customData: post.data.customData,
             link: `/blog/${post.slug}/`,
-            author: 'danielorihuelarodriguez@gmail.com'
+            author: 'danielorihuelarodriguez@gmail.com (Daniel Orihuela)'
         })),
     });
 }
