f2398715897baa4a7cd4835c70734f411f240487
dani <danielorihuelarodriguez@gmail.com>
2025-07-31 22:15:33 +0200

    fea: add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Personal website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ 
+            nodejs_24
+            pandoc
+          ];
+        };
+      });
+}
